92e086bd8c865d48f9abd43713a1a0d329968704
MadMrCrazy <madmrcrazy@gmail.com>
2018-05-07 12:03:26 +0930

    I got sick of doing the same thing over and over

---
+++ degree to radiants.py
@@ -0,0 +1,5 @@
+print("MadMrCrazy's Degree to Radiants program! Type 180 to exit!")
+degree = 1
+while degree != 180:
+    degree = int(input("insert degree:"))
+    print("pi/" + str(180 / degree) + " radiants")
